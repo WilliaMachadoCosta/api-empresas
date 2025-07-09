62ec8b345ddfe48751ee0e11349e307af2078225
williammachadocosta <williammachadocosta.trabalho@gmail.com>
2025-07-09 08:22:44 -0300

    feat: adiciona swagger-ui-express e ignora .env

--- routes/empresas.js
+++ routes/empresas.js
@@ -1,3 +1,20 @@
+/**
+ * @swagger
+ * tags:
+ *   name: Empresas
+ *   description: Endpoints para gestão de empresas
+ */
+
+/**
+ * @swagger
+ * /empresas:
+ *   get:
+ *     summary: Lista todas as empresas
+ *     tags: [Empresas]
+ *     responses:
+ *       200:
+ *         description: Lista de empresas retornada com sucesso
+ */
 const express = require('express');
 const router = express.Router();
 const controller = require('../controllers/empresaController');
--- routes/visitas.js
+++ routes/visitas.js
@@ -1,3 +1,20 @@
+/**
+ * @swagger
+ * tags:
+ *   name: Visitas
+ *   description: Endpoints para gestão de visitas
+ */
+
+/**
+ * @swagger
+ * /visitas:
+ *   get:
+ *     summary: Lista todas as visitas
+ *     tags: [Visitas]
+ *     responses:
+ *       200:
+ *         description: Lista de visitas retornada com sucesso
+ */
 const express = require('express');
 const router = express.Router();
 const controller = require('../controllers/visitaController');
